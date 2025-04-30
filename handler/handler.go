e65ca30bb7617f9612bc2e6a90f60884fe58aed0
janeefajjustin <janeefajjustin@gmail.com>
2025-04-30 09:25:35 +0530

    Rest API

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/janeefajjustin/task-1/db"
 	"github.com/janeefajjustin/task-1/models"
 )
 
@@ -16,7 +18,7 @@ func Login(context *gin.Context) {
 		return
 	}
 
-	//err = user.ValidateCredentials()
+	err = user.ValidateCredentials()
 	err = nil
 
 	if err != nil {
@@ -28,21 +30,21 @@ func Login(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "Login Successfull!"})
 }
 
-// func (u models.User) ValidateCredentials() error {
-// 	query := "SELECT id, password FROM users WHERE email=?"
-// 	row := db.DB.QueryRow(query, u.Email)
+func (u models.User) ValidateCredentials() error {
+	query := "SELECT id, password FROM users WHERE email=?"
+	row := db.DB.QueryRow(query, u.Email)
 
-// 	var retrivedPassword string
-// 	err := row.Scan(&u.ID, &retrivedPassword)
+	var retrivedPassword string
+	err := row.Scan(&u.ID, &retrivedPassword)
 
-// 	if err != nil {
-// 		return errors.New("credentials invalid")
-// 	}
+	if err != nil {
+		return errors.New("credentials invalid")
+	}
 
-// 	if u.Password != retrivedPassword {
-// 		return errors.New("credentials invalid")
-// 	}
+	if u.Password != retrivedPassword {
+		return errors.New("credentials invalid")
+	}
 
-// 	return nil
+	return nil
 
-// }
+}
